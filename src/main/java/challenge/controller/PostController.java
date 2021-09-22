90b8d5387c6e9ed0a81c46c654345f94d4c33cf3
Fernando Tadeu do Carmo Ramos Junior <fernandoramosjr86@gmail.com>
2021-09-22 18:33:01 -0300

    Implementação do serviço de criação de posts

--- src/main/java/challenge/controller/PostController.java
+++ src/main/java/challenge/controller/PostController.java
@@ -1,9 +1,16 @@
 package challenge.controller;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import challenge.model.Post;
+import challenge.model.User;
 import challenge.repository.PostRepository;
+import challenge.security.IAuthenticationFacade;
 
 @RestController
 public class PostController {
@@ -11,5 +18,15 @@ public class PostController {
 	@Autowired
 	private PostRepository repository;
 	
-	// ...
+	@Autowired
+    IAuthenticationFacade authenticationFacade;
+	
+	@PostMapping("/posts")
+	public Post newPost(@Valid @RequestBody Post post) {
+		User user = authenticationFacade.getUser();
+		post.setUser(user);
+		
+		return repository.save(post);
+		
+	}
 }
